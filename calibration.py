ace63a39a6a487f7eabe978c251a5c8f59ea234a
Pauline Lettinga <paulinelettinga@gmail.com>
2024-10-22 16:23:16 +0200

    Find H matrix with manually matching points

--- calibration.py
+++ calibration.py
@@ -1,55 +1,9 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Read image
-image = cv2.imread('veld.jpg')
-
-# Convert image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# # Use canny edge detection
-# edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-#
-# # Apply HoughLinesP method to
-# # to directly obtain line end points
-# lines_list = []
-# lines = cv2.HoughLinesP(
-#     edges,  # Input edge image
-#     1,  # Distance resolution in pixels
-#     np.pi / 180,  # Angle resolution in radians
-#     threshold=100,  # Min number of votes for valid line
-#     minLineLength=5,  # Min allowed length of line
-#     maxLineGap=10  # Max allowed gap between line for joining them
-# )
-#
-# # Iterate over points
-# for points in lines:
-#     # Extracted points nested in the list
-#     x1, y1, x2, y2 = points[0]
-#     # Draw the lines joing the points
-#     # On the original image
-#     cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-#     # Maintain a simples lookup list for points
-#     lines_list.append([(x1, y1), (x2, y2)])
-
-# Initialize SIFT detector
-sift = cv2.SIFT_create()
-
-# Detect SIFT keypoints and descriptors
-keypoints, descriptors = sift.detectAndCompute(gray, None)
-
-# Draw keypoints on the image (for visualization)
-sift_image = cv2.drawKeypoints(image, keypoints, None)
-
-# Show the image with keypoints
-cv2.imshow('SIFT Keypoints', sift_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# cv2.imshow('detectedLines', image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+from helper import Helper
+from helper import Line
+Help = Helper()
 
 # define world frame
 # total frame 780x575 dm)
@@ -73,36 +27,35 @@ world_keypoints = [
     (299, 50), (299, 105), (482, 50), (482, 105)
 ]
 
-# # Plot a line given two points
-# def plot_line(points, label):
-#     x_values = [points[0][0], points[1][0]]
-#     y_values = [points[0][1], points[1][1]]
-#     plt.plot(x_values, y_values, label=label)
-#
-# # Create a plot
-# plt.figure
-#
-# # Plot each line on the field
-# plot_line(outer_left, 'Outer Left')
-# plot_line(outer_right, 'Outer Right')
-# plot_line(mid, 'Mid Line')
-# plot_line(back, 'Back Line')
-# plot_line(penalty, 'Penalty Line')
-# plot_line(penalty_left, 'Penalty Left')
-# plot_line(penalty_right, 'Penalty Right')
-# plot_line(goal, 'Goal Line')
-# plot_line(goal_left, 'Goal Left')
-# plot_line(goal_right, 'Goal Right')
-#
-# # Customize the plot
-# plt.xlim(0, 780)  # Adjust based on expected field size
-# plt.ylim(0, 575)  # Adjust based on expected field size
-# plt.gca().invert_yaxis()  # Invert Y axis to match typical soccer field layout (origin at top-left)
-# plt.xlabel("X coordinate (in dm)")
-# plt.ylabel("Y coordinate (in dm)")
-# plt.title("Soccer Field Lines")
-# plt.legend(loc="lower right")
-#
-# # Show the plot
-# plt.grid(True)
-# plt.show()
+# Read image
+img = cv2.imread('frame.png', cv2.IMREAD_COLOR) # road.png is the filename
+
+satAdjusted = Help.satAdjHSV(img, 5)
+# plt.hist(satAdjusted.ravel(),256,[0,256]); plt.show()
+mask = Help.fieldMask(satAdjusted)
+masked = cv2.bitwise_and(img, img, mask=mask)
+
+lines = list(map(Line, Help.houghLines(masked)))
+linesGrouped = Help.lineGrouper(lines)
+
+# Draw lines on the image
+for index, group in enumerate(linesGrouped):
+    color = (255, 0, 255)
+    if index == 0:
+        color = (255, 0, 0)
+    elif index == 1:
+        color = (0, 0, 255)
+    for line in group:
+        cv2.line(img, -line, +line, color, 3)
+
+intersections = Help.find_field_intersections(linesGrouped, mask)
+intersections.pop(0)
+img = Help.draw_intersections(img, intersections)
+matched_world_points = [(189, 215), (299, 105), (50, 50), (189, 50), (299, 50)]
+
+H, mask = cv2.findHomography(np.asarray(matched_world_points), np.asarray(intersections))
+print(H)
+# Show result
+# cv2.imshow("blank", cv2.cvtColor(satAdjusted, cv2.COLOR_HSV2BGR))
+cv2.imshow("blank", img)
+cv2.waitKey(5000)
